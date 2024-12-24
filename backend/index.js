789e7245e2160e390a2035d920a5d438cbf21546
vikas-saini-7 <vikas289888@gmail.com>
2024-12-24 23:55:22 +0530

    added auth

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,31 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 
+const authRoutes = require("./routes/authRoutes.js");
+
 const app = express();
-const prisma = new PrismaClient();
 
 //json
 app.use(express.json());
 
 //cors
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type"); //, Authorization
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
+// app.use(cors());
 
 //test
 app.get("/test", (req, res) => {
-  res.status(200).json({ message: "API working..." });
+  res.status(200).json({ message: "API working perfectly..." });
 });
 
+app.use("/auth", authRoutes);
+
 //listen
-const PORT = process.env.PORT || 9000;
-app.listen(9000, () => {
+const PORT = process.env.PORT || 8000;
+app.listen(8000, () => {
   console.log(`Server running on port ${PORT}`);
 });
