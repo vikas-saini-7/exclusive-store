78f13815fcb09946ef4ec466ee54f736a815c8cc
vikas-saini-7 <vikas289888@gmail.com>
2025-03-20 12:39:57 +0530

    backend delete

--- backend/routes/statsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const statsController = require("../controllers/statsController.js");
-
-// get home stats
-router.get("/", statsController.getHomeStats);
-
-module.exports = router;
